e0bbd91050678fb3ba285269689195d4b2d8a277
ShangYJQ <421207553@qq.com>
2026-05-08 08:49:57 +0000

    refactor(fish): migrate config to home-manager

--- home.nix
+++ home.nix
@@ -63,6 +63,9 @@
         JAVA_HOME = "${pkgs.jdk17.home}";
 
         NODE_OPTIONS = "--max-old-space-size=8192";
+
+        EDITOR = "nvim";
+        FZF_DEFAULT_COMMAND = "fd --type f";
       };
 
       home.file.".config/nvim" = {
@@ -83,11 +86,47 @@
       programs.fish = {
         enable = true;
 
+        shellAbbrs = {
+          qkgit = "git add .; and aicommits -y; and git push";
+          gpl = "git pull";
+          gph = "git push";
+          g2p = "git pull; and git push";
+          ga = "git add .";
+          gr = "git restore .";
+          gs = "git status --short";
+          cs = "codex-switch";
+          rebuild = "nixos-rebuild switch --flake /etc/nixos#nixos";
+        };
+
+        shellAliases = {
+          l = "command eza --icons -l --group-directories-first";
+          ls = "command eza --icons --group-directories-first";
+          ll = "command eza --icons -l --git --header --total-size --time-style=long-iso";
+          la = "command eza --icons -l -a --git --header --total-size --time-style=long-iso";
+          lt = "command eza --icons --tree --level 3";
+          c = "clear";
+        };
+
+        functions = {
+          y = ''
+            set tmp (mktemp -t "yazi-cwd.XXXXXX")
+            command yazi $argv --cwd-file="$tmp"
+            if read -z cwd <"$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+                builtin cd -- "$cwd"
+            end
+            rm -f -- "$tmp"
+          '';
+        };
+
         interactiveShellInit = ''
-          source /etc/nixos/fish/config.fish
+          if test "$TERM" = "xterm-ghostty"
+            set -gx TERM xterm-256color
+          end
+
           source /etc/nixos/fish/fish_prompt.fish
           source /etc/nixos/fish/fish_frozen_theme.fish
         '';
+
       };
 
       programs.zellij = {
